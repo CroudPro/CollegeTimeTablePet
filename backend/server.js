c23969d514876375a187e73ef252bdf240c65eaa
CroudPro <alex9693.mr@gmail.com>
2021-03-02 01:10:49 +0300

    Heroku tryUp

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require('path')
 const connectDB = require("./config/db.js");
 const lessonsRoutes = require('./routes/lessonsRoutes')
 const groupsRoutes = require('./routes/groupRoutes')
@@ -12,6 +13,18 @@ app.use(express.json());
 
 app.use('/api/lessons',lessonsRoutes);
 app.use('/api/groups',groupsRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,'../frontend/build')));
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname,'../frontend','build','index.html'))
+    })
+}else {
+    app.get("/",(req,res) => {
+        res.send("Api Running");
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`)
